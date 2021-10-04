091a45588de515112370e5b6b0ec841db7f96767
Alin Popa <61394189+skilly707@users.noreply.github.com>
2021-10-04 09:59:31 +0300

    Create db.js
    Db for fakeAPI

---
+++ db.js
@@ -0,0 +1,70 @@
+{
+ "buttonsTab": [
+   {
+    "name": "Luni",
+    "type": "BUTTON_TYPE_DAY"
+   },
+   {
+    "name": "Marti",
+    "type": "BUTTON_TYPE_DAY"
+   },
+   {
+    "name": "Miercuri",
+    "type": "BUTTON_TYPE_DAY"
+   },
+   {
+    "name": "Joi",
+    "type": "BUTTON_TYPE_DAY"
+   },
+   {
+    "name": "Vineri",
+    "type": "BUTTON_TYPE_DAY"
+   },
+   {
+    "name": "Rezumat",
+    "type": "BUTTON_TYPE_SUMMARY"
+   }
+   ],
+ "activities": [
+   {
+    "index": "0",
+    "name": "Alege activitatea dorita",
+    "energy": "0"
+   },
+   {
+    "index": "1",
+    "name": "Alergare usoara",
+    "energy": "260"
+   },
+   {
+    "index": "2",
+    "name": "Karate",
+    "energy": "520"
+   },
+   {
+    "index": "3",
+    "name": "Tenis",
+    "energy": "700"
+   },
+   {
+    "index": "4",
+    "name": "Inot",
+    "energy": "817"
+   },
+   {
+    "index": "5",
+    "name": "Fotbal",
+    "energy": "700"
+   },
+   {
+    "index": "6",
+    "name": "Handbal",
+    "energy": "500"
+   },
+   {
+    "index": "7",
+    "name": "Volei",
+    "energy": "444"
+   }
+   ]
+}
